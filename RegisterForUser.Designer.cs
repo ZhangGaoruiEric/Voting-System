d0a3f18f582a9ef5e01363dc3fc423517803c51c
ZhangGaoruiEric <47535527+ZhangGaoruiEric@users.noreply.github.com>
2019-03-13 22:30:24 +0000

    Add files via upload

---
+++ RegisterForUser.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace VotingSystem
+{
+    partial class RegisterForUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserName_lab = new System.Windows.Forms.Label();
+            this.Password_lab = new System.Windows.Forms.Label();
+            this.UserName_txt = new System.Windows.Forms.TextBox();
+            this.Password_txt = new System.Windows.Forms.TextBox();
+            this.Telephone_lab = new System.Windows.Forms.Label();
+            this.Email_lab = new System.Windows.Forms.Label();
+            this.Tel_txt = new System.Windows.Forms.TextBox();
+            this.Email_txt = new System.Windows.Forms.TextBox();
+            this.Role_lab = new System.Windows.Forms.Label();
+            this.OK_btn = new System.Windows.Forms.Button();
+            this.Exit_btn = new System.Windows.Forms.Button();
+            this.UserId_lab = new System.Windows.Forms.Label();
+            this.UserId_txt = new System.Windows.Forms.TextBox();
+            this.Home_btn = new System.Windows.Forms.Button();
+            this.Reason_lab = new System.Windows.Forms.Label();
+            this.Reason_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Role_comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // UserName_lab
+            // 
+            this.UserName_lab.AutoSize = true;
+            this.UserName_lab.Location = new System.Drawing.Point(66, 90);
+            this.UserName_lab.Name = "UserName_lab";
+            this.UserName_lab.Size = new System.Drawing.Size(79, 15);
+            this.UserName_lab.TabIndex = 0;
+            this.UserName_lab.Text = "UserName:";
+            // 
+            // Password_lab
+            // 
+            this.Password_lab.AutoSize = true;
+            this.Password_lab.Location = new System.Drawing.Point(66, 157);
+            this.Password_lab.Name = "Password_lab";
+            this.Password_lab.Size = new System.Drawing.Size(79, 15);
+            this.Password_lab.TabIndex = 1;
+            this.Password_lab.Text = "Password:";
+            // 
+            // UserName_txt
+            // 
+            this.UserName_txt.Location = new System.Drawing.Point(206, 80);
+            this.UserName_txt.Name = "UserName_txt";
+            this.UserName_txt.Size = new System.Drawing.Size(145, 25);
+            this.UserName_txt.TabIndex = 2;
+            // 
+            // Password_txt
+            // 
+            this.Password_txt.Location = new System.Drawing.Point(206, 157);
+            this.Password_txt.Name = "Password_txt";
+            this.Password_txt.Size = new System.Drawing.Size(145, 25);
+            this.Password_txt.TabIndex = 3;
+            // 
+            // Telephone_lab
+            // 
+            this.Telephone_lab.AutoSize = true;
+            this.Telephone_lab.Location = new System.Drawing.Point(66, 225);
+            this.Telephone_lab.Name = "Telephone_lab";
+            this.Telephone_lab.Size = new System.Drawing.Size(87, 15);
+            this.Telephone_lab.TabIndex = 4;
+            this.Telephone_lab.Text = "Telephone:";
+            // 
+            // Email_lab
+            // 
+            this.Email_lab.AutoSize = true;
+            this.Email_lab.Location = new System.Drawing.Point(66, 292);
+            this.Email_lab.Name = "Email_lab";
+            this.Email_lab.Size = new System.Drawing.Size(55, 15);
+            this.Email_lab.TabIndex = 5;
+            this.Email_lab.Text = "Email:";
+            // 
+            // Tel_txt
+            // 
+            this.Tel_txt.Location = new System.Drawing.Point(206, 214);
+            this.Tel_txt.Name = "Tel_txt";
+            this.Tel_txt.Size = new System.Drawing.Size(145, 25);
+            this.Tel_txt.TabIndex = 6;
+            // 
+            // Email_txt
+            // 
+            this.Email_txt.Location = new System.Drawing.Point(206, 282);
+            this.Email_txt.Name = "Email_txt";
+            this.Email_txt.Size = new System.Drawing.Size(145, 25);
+            this.Email_txt.TabIndex = 7;
+            // 
+            // Role_lab
+            // 
+            this.Role_lab.AutoSize = true;
+            this.Role_lab.Location = new System.Drawing.Point(66, 358);
+            this.Role_lab.Name = "Role_lab";
+            this.Role_lab.Size = new System.Drawing.Size(47, 15);
+            this.Role_lab.TabIndex = 8;
+            this.Role_lab.Text = "Role:";
+            // 
+            // OK_btn
+            // 
+            this.OK_btn.Location = new System.Drawing.Point(148, 464);
+            this.OK_btn.Name = "OK_btn";
+            this.OK_btn.Size = new System.Drawing.Size(88, 23);
+            this.OK_btn.TabIndex = 10;
+            this.OK_btn.Text = "Register";
+            this.OK_btn.UseVisualStyleBackColor = true;
+            this.OK_btn.Click += new System.EventHandler(this.OK_btn_Click);
+            // 
+            // Exit_btn
+            // 
+            this.Exit_btn.Location = new System.Drawing.Point(802, 463);
+            this.Exit_btn.Name = "Exit_btn";
+            this.Exit_btn.Size = new System.Drawing.Size(75, 23);
+            this.Exit_btn.TabIndex = 11;
+            this.Exit_btn.Text = "Exit";
+            this.Exit_btn.UseVisualStyleBackColor = true;
+            this.Exit_btn.Click += new System.EventHandler(this.Exit_btn_Click);
+            // 
+            // UserId_lab
+            // 
+            this.UserId_lab.AutoSize = true;
+            this.UserId_lab.Location = new System.Drawing.Point(69, 32);
+            this.UserId_lab.Name = "UserId_lab";
+            this.UserId_lab.Size = new System.Drawing.Size(71, 15);
+            this.UserId_lab.TabIndex = 14;
+            this.UserId_lab.Text = "User Id:";
+            // 
+            // UserId_txt
+            // 
+            this.UserId_txt.Location = new System.Drawing.Point(206, 32);
+            this.UserId_txt.Name = "UserId_txt";
+            this.UserId_txt.Size = new System.Drawing.Size(145, 25);
+            this.UserId_txt.TabIndex = 15;
+            // 
+            // Home_btn
+            // 
+            this.Home_btn.Location = new System.Drawing.Point(472, 463);
+            this.Home_btn.Name = "Home_btn";
+            this.Home_btn.Size = new System.Drawing.Size(75, 23);
+            this.Home_btn.TabIndex = 16;
+            this.Home_btn.Text = "Home";
+            this.Home_btn.UseVisualStyleBackColor = true;
+            this.Home_btn.Click += new System.EventHandler(this.Home_btn_Click);
+            // 
+            // Reason_lab
+            // 
+            this.Reason_lab.AutoSize = true;
+            this.Reason_lab.Location = new System.Drawing.Point(502, 224);
+            this.Reason_lab.Name = "Reason_lab";
+            this.Reason_lab.Size = new System.Drawing.Size(63, 15);
+            this.Reason_lab.TabIndex = 17;
+            this.Reason_lab.Text = "Reason:";
+            // 
+            // Reason_txt
+            // 
+            this.Reason_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Reason_txt.Location = new System.Drawing.Point(571, 72);
+            this.Reason_txt.Multiline = true;
+            this.Reason_txt.Name = "Reason_txt";
+            this.Reason_txt.Size = new System.Drawing.Size(343, 218);
+            this.Reason_txt.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(577, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 30);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "If you want to be popular, please write \r\nyour reason for being a candidate";
+            // 
+            // Role_comboBox
+            // 
+            this.Role_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Role_comboBox.FormattingEnabled = true;
+            this.Role_comboBox.Items.AddRange(new object[] {
+            "Canidate",
+            "Elector"});
+            this.Role_comboBox.Location = new System.Drawing.Point(206, 350);
+            this.Role_comboBox.Name = "Role_comboBox";
+            this.Role_comboBox.Size = new System.Drawing.Size(145, 23);
+            this.Role_comboBox.TabIndex = 85;
+            // 
+            // RegisterForUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1033, 540);
+            this.Controls.Add(this.Role_comboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Reason_txt);
+            this.Controls.Add(this.Reason_lab);
+            this.Controls.Add(this.Home_btn);
+            this.Controls.Add(this.UserId_txt);
+            this.Controls.Add(this.UserId_lab);
+            this.Controls.Add(this.Exit_btn);
+            this.Controls.Add(this.OK_btn);
+            this.Controls.Add(this.Role_lab);
+            this.Controls.Add(this.Email_txt);
+            this.Controls.Add(this.Tel_txt);
+            this.Controls.Add(this.Email_lab);
+            this.Controls.Add(this.Telephone_lab);
+            this.Controls.Add(this.Password_txt);
+            this.Controls.Add(this.UserName_txt);
+            this.Controls.Add(this.Password_lab);
+            this.Controls.Add(this.UserName_lab);
+            this.Name = "RegisterForUser";
+            this.Text = "Register";
+            this.Load += new System.EventHandler(this.RegisterForUser_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UserName_lab;
+        private System.Windows.Forms.Label Password_lab;
+        private System.Windows.Forms.TextBox UserName_txt;
+        private System.Windows.Forms.TextBox Password_txt;
+        private System.Windows.Forms.Label Telephone_lab;
+        private System.Windows.Forms.Label Email_lab;
+        private System.Windows.Forms.TextBox Tel_txt;
+        private System.Windows.Forms.TextBox Email_txt;
+        private System.Windows.Forms.Label Role_lab;
+        private System.Windows.Forms.Button OK_btn;
+        private System.Windows.Forms.Button Exit_btn;
+        private System.Windows.Forms.Label UserId_lab;
+        private System.Windows.Forms.TextBox UserId_txt;
+        private System.Windows.Forms.Button Home_btn;
+        private System.Windows.Forms.Label Reason_lab;
+        private System.Windows.Forms.TextBox Reason_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Role_comboBox;
+    }
+}
---
+++ RegisterForUser.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace VotingSystem
+{
+    public partial class RegisterForUser : Form
+    {
+        public RegisterForUser()
+        {
+            InitializeComponent();
+            
+        }
+
+       
+
+        string strcon, strsql;
+        SqlConnection mycon;
+        SqlCommand command;
+        //Link the database
+        private bool DBConnect()
+        {
+
+           
+            try
+            {
+                strcon = "Data Source=DESKTOP-BAERS9T\\SQLEXPRESS;Initial Catalog=Voting;Integrated Security=True";
+                mycon = new SqlConnection(strcon);
+                mycon.Open();
+               
+                MessageBox.Show("Link datebase is succesfully");
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Link datebase is not succesfully");
+                return false;
+            }
+            //Check the database
+        }
+
+       
+
+        private void RegisterForUser_Load(object sender, EventArgs e)
+        {
+            
+            this.Resize += new EventHandler(Form1_Resize);
+
+            X = this.Width;
+            Y = this.Height;
+
+
+            setTag(this);
+            Form1_Resize(new object(), new EventArgs());
+        }
+
+        private float X;// set a coordinate X
+
+        private float Y;// set a coordinate Y
+
+        private void setTag(Control cons)
+        {
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ":" + con.Height + ":" + con.Left + ":" + con.Top + ":" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                    setTag(con);
+                //  Width + Height +Left + Top + Font 
+            }
+        }
+        private void setControls(float newx, float newy, Control cons)
+        {
+            foreach (Control con in cons.Controls)
+            {
+
+                string[] mytag = con.Tag.ToString().Split(new char[] { ':' });
+                float a = Convert.ToSingle(mytag[0]) * newx;
+                con.Width = (int)a;
+                a = Convert.ToSingle(mytag[1]) * newy;
+                con.Height = (int)(a);
+                a = Convert.ToSingle(mytag[2]) * newx;
+                con.Left = (int)(a);
+                a = Convert.ToSingle(mytag[3]) * newy;
+                con.Top = (int)(a);
+                Single currentSize = Convert.ToSingle(mytag[4]) * Math.Min(newx, newy);
+                con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                if (con.Controls.Count > 0)
+                {
+                    setControls(newx, newy, con);
+                }
+            }
+            // Store the original form and font size
+        }
+
+        void Form1_Resize(object sender, EventArgs e)
+        {
+            float newx = (this.Width) / X;
+            float newy = this.Height / Y;
+            setControls(newx, newy, this);
+            this.Text = this.Width.ToString() + " " + this.Height.ToString();
+            // Reset the font when zooming in on the form
+
+        }
+
+        private void Exit_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Home_btn_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            this.Hide();
+            login.ShowDialog(this);
+        }
+
+       
+
+       
+
+
+
+        private void OK_btn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(UserName_txt.Text))
+            {
+                MessageBox.Show("Enter your name please");
+                return;
+            }
+            
+            if (string.IsNullOrEmpty(Password_txt.Text))
+            {
+                MessageBox.Show("Enter your password please");
+                return;
+            }
+
+            //if (string.IsNullOrEmpty(Role_comboBox.Text))
+            //{
+            //    MessageBox.Show("Choose your role please");
+            //    return;
+            //}
+
+            if (DBConnect())
+            {
+                strsql = string.Format("insert into VotingUsers(UserId,UserName,Password,Telephone,Email,Role,Reason) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}')", UserId_txt.Text,UserName_txt.Text, Password_txt.Text, Tel_txt.Text, Email_txt.Text, Role_comboBox.Text,Reason_txt);
+                //Add information in the database
+                MessageBox.Show(strsql);
+                command = new SqlCommand(strsql, mycon);
+                try
+                {
+                    command.ExecuteScalar();
+                    MessageBox.Show("Successfully register.");
+
+                }
+                catch
+                {
+                    MessageBox.Show("Register Error.");
+                }
+                //Check register
+                finally
+                {
+                    mycon.Close();
+                }
+                //Close the database
+            }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+
+    }
+
